15b768d444ed7ee67185663f3777be036b61bb0e
lookieAU <plabandatta2015@gmail.com>
2022-01-18 11:37:03 +0530

    Trial 1

--- index.js
+++ index.js
@@ -28,14 +28,44 @@ function playRound(playerSelection,computerSelection){
     }
 }
 function game(){
-    let count = 4;
+    let count = 5,win = 0, loss=0;
     for(i=0;i<=count;i++){
-        const playerSelection = prompt("Rock, Paper or Scissors?");
+        // const playerSelection = prompt("Rock, Paper or Scissors?");
         const computerSelection = computerPlay();
-        console.log(playRound(playerSelection,computerSelection))
+        const buttons = document.querySelectorAll('button');
+        // console.log(buttons);
+        buttons.forEach((button)=>{
+            button.addEventListener('click',()=>{
+                // const div = document.createElement('div');
+                const result = playRound(button.textContent,computerSelection);
+                console.log(result[4]);
+                if (result[4]=='W'){
+                    win +=1;
+                }
+                else if(result[4]=='L'){
+                    loss +=1;
+                }
+            });
+        });
     }
+    const div = document.createElement('div');
+    div.classList.add('result');
+    div.setAttribute('style','align-text: center;');
+    if(win>loss){
+        div.textContent = "You are the winner of the 5 rounds played!"
+    }
+    else if(loss>win){
+        div.textContent = "You lose the game after 5 rounds unfortunately!"
+    }
+    else{
+        div.textContent = "Its a draw! Up for a rematch? Just reload the page."
+    }
+    const container = document.querySelector('body');
+    container.appendChild(div);
 }
 
 prompt("Dare to play with me - I am your PC");
 game();
+
+
 
